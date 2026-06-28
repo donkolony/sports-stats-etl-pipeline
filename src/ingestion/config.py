020e1cc4616d24a820bd9bdcb0d9079d09795146
Don Kolony <donkolony@gmail.com>
2026-06-28 15:40:27 +0200

    feat: create config module to securely load api environment variables

--- src/ingestion/config.py
+++ src/ingestion/config.py
@@ -4,3 +4,19 @@
 Handles all configurations
 Reads environmental variables and stores constants
 """
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Access the variables
+API_KEY = os.getenv("FOOTBALL_DATA_API_KEY")
+
+# Guard Clause: Raise error if api key is missing
+if not API_KEY:
+    raise ValueError("FOOTBALL_DATA_API_KEY is not set. Please check your .env file.")
+
+# URL
+BASE_URL = "https://api.football-data.org/v4/"
