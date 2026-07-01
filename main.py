96a665124f2fdaa9e6cce9d79ddb23138dd35ba8
Don Kolony <donkolony@gmail.com>
2026-07-01 22:10:16 +0200

    feat: implement api_client with dynamic endpoints and fail fast error handling

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from sports-stats-etl-pipeline!")
-
+from src.ingestion.api_client import fetch_api_data
 
 if __name__ == "__main__":
-    main()
+    # Test the endpoints
+    match_endpoint = "matches"
+
+    print(f"Fetching data from endpoint: {match_endpoint}...")
+    res = fetch_api_data(match_endpoint)
+
+    print(res["matches"])
--- src/ingestion/api_client.py
+++ src/ingestion/api_client.py
@@ -1,6 +1,34 @@
-# Infrastructure: HTTP communication
+import requests
 
-"""
-Handles authentication (makes the GET requests)
-Deals with pagination
-"""
+from src.ingestion.config import API_KEY, BASE_URL
+
+# Football-Data API auth header
+headers = {"X-Auth-Token": API_KEY}
+
+
+def fetch_api_data(endpoint: str) -> dict:
+    """
+    Fetches JSON data from the Football-Data.org API using a dynamically provided endpoint
+
+    Args:
+        endpoint (str): The specific API path to append to the base URL
+
+    Raises:
+        RuntimeError: If the HTTP request fails (e.g., 404 Not Found, 429 Too Many Requests).
+                      ensuring the pipeline crashes immediately rather than passing bad data.
+
+    Returns:
+        dict: The raw API response parsed into a Python dictionary
+    """
+
+    full_url = f"{BASE_URL}{endpoint}"
+
+    try:
+        res = requests.get(full_url, headers=headers)
+        res.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        # Chain the exception to provide a custom, readable error message for Airflow
+        # while preserving the original traceback context
+        raise RuntimeError(f"API Request failed to fetch data. Details: {e}") from e
+
+    return res.json()
