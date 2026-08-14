28ebd5131bb0ad34bf6789bf8de910da86cdddde
Don Kolony <donkolony@gmail.com>
2026-08-14 21:25:45 +0200

    [DE-005] feat(runner): implement the the orchestrator that integrates the different layers
    This is the big boss, the glue that puts everything together. It is
    blind to the logic or doesnt care at all of the other layers but just
    knows which item to pass to it and lets them handle the rest. It uses
    argparse to handle command line arguments which is similar to what
    Apache Airflow will do in later stage of the pipeline. Might make use of
    Typer (the god of CLI) instead of argparse

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from datetime import UTC, datetime
 
 from src.ingestion.api_client import fetch_api_data
-from src.ingestion.duckdb_client import load_raw_data
 from src.ingestion.local_storage import save_raw_data
 
 
 def main() -> None:
+
     print("Hello from sport-stats-etl-pipeline!")
+
+    competition = "PL"
+    entity = "matches"
+
+    api_endpoint = f"competitions/{competition}/{entity}"
+
+    today: datetime = datetime.now(UTC)
+
+    print(f"Fetching {entity} for {competition}")
+
+    # Extract
+    raw_data: dict = fetch_api_data(endpoint=api_endpoint)
+
+    # Load
+    save_raw_data(
+        data=raw_data, competition=competition, entity=entity, execution_date=today
+    )
+
+    print("Successfully loaded!")
 
 
 if __name__ == "__main__":
     main()
-    match_endpoint: str = "competitions/PL/teams"  # TODO make endpoints dynamic
-    today: datetime = datetime.now(UTC)
-
-    # print(match_endpoint)
-    # print(match_table)
-
-    raw_data: dict = fetch_api_data(endpoint=match_endpoint)
-
-    save_raw_data(data=raw_data, entity=match_endpoint, execution_date=today)
-
-    load_raw_data(entity=match_endpoint, execution_date=today)
