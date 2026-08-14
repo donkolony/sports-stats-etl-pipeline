19471d93433f759ac9efa7aacebdc28366b51666
Don Kolony <donkolony@gmail.com>
2026-08-14 21:07:42 +0200

    Revert "[DE-004] refactor(local_storage): update function to accept a new parameter"
    This reverts commit 3b268d29e4d7ebaaedcd223f124856c330f7b8e0.

--- src/ingestion/local_storage.py
+++ src/ingestion/local_storage.py
@@ -3,43 +3,28 @@ from datetime import datetime
 from pathlib import Path
 
 
-def save_raw_data(
-    data: dict, competition: str, entity: str, execution_date: datetime
-) -> str:
+def save_raw_data(data: dict, entity: str, execution_date: datetime) -> None:
     """
-    Saves the raw data dictionary as a JSON file in a date-partitioned data lake.
+    Saves the raw data dictionary from the api client as a JSON file in a date partitioned data lake.
 
     Args:
         data (dict): The raw API response payload to persist
-        competition (str): The league code (e.g., "PL", "BL1")
-        entity (str): The data domain being saved (e.g. "matches", "standings", "teams")
-        execution_date (datetime): The Airflow logical date, used to determine the partition path
 
-    Returns:
-        str: Hands off state (file path) 
+        entity (str): The data domain being saved (e.g. "matches", "standings")
+
+        execution_date (datetime): The Airflow logical date, used to determine the partition path
     """
 
     year: str = execution_date.strftime("%Y")
     month: str = execution_date.strftime("%m")
     day: str = execution_date.strftime("%d")
 
-    dir_path = (
-        Path("storage")
-        / "raw"
-        / "competitions"
-        / competition
-        / entity
-        / year
-        / month
-        / day
-    )
+    dir_path = Path("storage") / "raw" / entity / year / month / day
 
     # Defensively create the directory structure
     dir_path.mkdir(parents=True, exist_ok=True)
 
-    file_path = dir_path / "raw_data.json"
+    file_path: str = dir_path / "data.json"  # TODO replace filename
 
     with open(file=file_path, mode="w") as f:
         json.dump(obj=data, fp=f, indent=4)
-
-    return str(dir_path)
