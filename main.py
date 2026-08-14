92699ab99e88342b2a86e754d6881187126d1389
Don Kolony <donkolony@gmail.com>
2026-08-14 17:08:20 +0200

    [DE-004] feat: implement the duckdb client to load data
    Implemented the load raw data that is responsible for loading the data
    inside the data lake onto the warehouse where transform will ocurr using
    an idempotent delete-insert strategy. This method can be improved
    further later if there is time as I want to isolate it inside a
    try-except block as right now it creates the db even if there is an
    error, it basically crashes but still creates the db
    
    closes #4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
 from src.ingestion.api_client import fetch_api_data
+from src.ingestion.duckdb_client import load_raw_data
 from src.ingestion.local_storage import save_raw_data
 
 
@@ -11,8 +12,13 @@ def main() -> None:
 if __name__ == "__main__":
     main()
     match_endpoint: str = "competitions/PL/teams"  # TODO make endpoints dynamic
-    today: datetime = datetime.now()
+    today: datetime = datetime.now(UTC)
+
+    # print(match_endpoint)
+    # print(match_table)
 
     raw_data: dict = fetch_api_data(endpoint=match_endpoint)
 
     save_raw_data(data=raw_data, entity=match_endpoint, execution_date=today)
+
+    load_raw_data(entity=match_endpoint, execution_date=today)
