7c885f5d6ffb8c940d6142189d84c9aad02d7cd3
Don Kolony <donkolony@gmail.com>
2026-08-16 12:39:04 +0200

    [DE-18] refactor: minor change to the url variable name
    Changed name from full_url to url to keep it simple

--- src/ingestion/api_client.py
+++ src/ingestion/api_client.py
@@ -8,10 +8,10 @@ headers: dict = {HEADER_KEY: API_KEY}
 
 def fetch_api_data(endpoint: str) -> dict:
 
-    full_url: str = f"{BASE_URL}{endpoint}"
+    url: str = f"{BASE_URL}{endpoint}"
 
     try:
-        response = requests.get(url=full_url, headers=headers)
+        response = requests.get(url=url, headers=headers)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         raise RuntimeError(f"API failed to fetch data. Details {e}") from e
