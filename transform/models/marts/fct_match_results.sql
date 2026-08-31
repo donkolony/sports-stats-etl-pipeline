8d6622cd72ae75cb9cf0574a02f483f3d024aa73
Don Kolony <donkolony@gmail.com>
2026-08-31 19:28:26 +0200

    [DE-023] refactor: include match scores in fct_match_results to meet acceptance criteria
    I initally forgot to include the match scores and only added team ids to
    meet the issue acceptance criteria

--- transform/models/marts/fct_match_results.sql
+++ transform/models/marts/fct_match_results.sql
@@ -1,7 +1,10 @@
 SELECT
     matches.match_id,
+    matches.match_status,
     home.team_name as home_team,
     away.team_name as away_team,
+    matches.home_goals,
+    matches.away_goals
 FROM {{ ref('stg_matches') }} as matches -- table name
 LEFT JOIN {{ ref('dim_teams') }} as home -- queryed table
     on matches.home_team_id = home.team_id
