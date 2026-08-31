5a21a671913944dadfb6663ffdfed08bcc07c525
Don Kolony <donkolony@gmail.com>
2026-08-31 19:12:45 +0200

    [DE-023] feat: build dim_teams and fct_match_results marts
    - Implement dbt data quality tests for staging models
    - Create dim_teams as the core dimensional source of truth
    - Create fct_match_results using a double left join to resolve home and
    away team IDs

---
+++ transform/models/marts/fct_match_results.sql
@@ -0,0 +1,9 @@
+SELECT
+    matches.match_id,
+    home.team_name as home_team,
+    away.team_name as away_team,
+FROM {{ ref('stg_matches') }} as matches -- table name
+LEFT JOIN {{ ref('dim_teams') }} as home -- queryed table
+    on matches.home_team_id = home.team_id
+LEFT JOIN {{ 'dim_teams' }} as away
+    on matches.away_team_id = away.team_id
